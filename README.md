7dc71cf63d166578817bb1d03e59529b8228ad3f
Diaresta <TheAndrewRobles@yahoo.com>
2021-03-02 01:34:07 -0500

    home page styling

--- README.md
+++ README.md
@@ -7,6 +7,13 @@ Possible names:
 1. Shine: Slippi Stats Parser
 2. Shine: SSBM Stats Parser/Generator
 
+# TODO
+
+- [ ] Change inputs to form with button being submit
+- [ ] Change xhr request to fetch()
+- [ ] Style Stats Page
+- [ ] JS for stats page
+
 # About
 
 This work is purely educational, and isn't approved, endorsed, or affiliated with [Slippi](https://slippi.gg/). All Slippi assets/name belong to Slippi, and can/will be removed upon request.
--- static/scripts/upload-box.js
+++ static/scripts/upload-box.js
@@ -1,8 +1,10 @@
 const FILE_NAME = document.getElementById('fileName');
+const UPLOAD_BOX = document.getElementById('upload-box');
+const FILE_INPUT = document.getElementById('file-input');
+const CLEAR_BUTTON = document.getElementById('clearButton');
+const STATS_BUTTON = document.getElementById('statsButton');
 
 function uploadStyling() {
-  var uploadBox = document.getElementById('upload-box');
-
   var uploadFile = function (files) {
     var formData = new FormData(),
       xhr = new XMLHttpRequest(),
@@ -17,36 +19,52 @@ function uploadStyling() {
       console.log(data);
     };
 
-    xhr.open('POST', 'static/scripts/script.js');
+    xhr.open('POST', '/file-upload');
     xhr.send(formData);
 
     console.log(files);
   };
 
   // Stops selected file from opening in browser
-  uploadBox.ondrop = function (e) {
+  UPLOAD_BOX.ondrop = function (e) {
     e.preventDefault();
     // this.className = 'upload-box';
     uploadFile(e.dataTransfer.files);
 
-    // console.log(e.dataTransfer);
     console.log(e.dataTransfer.files[0].name);
 
     FILE_NAME.innerHTML = e.dataTransfer.files[0].name + ' - time';
     FILE_NAME.style.fontStyle = 'italic';
+    STATS_BUTTON.style.border = '2px solid #21ba45';
+    STATS_BUTTON.style.cursor = 'pointer';
   };
 
   // File upload styling hovering
-  uploadBox.ondragover = function () {
+  UPLOAD_BOX.ondragover = function () {
     this.className = 'upload-box dragover';
     return false;
   };
 
   // File upload styling non-hovering
-  uploadBox.ondragleave = function () {
+  UPLOAD_BOX.ondragleave = function () {
     this.className = 'upload-box';
     return false;
   };
+
+  // Upload File by Clicking
+  UPLOAD_BOX.onclick = function () {
+    FILE_INPUT.click();
+  };
+
+  // Clears styling/inputs when clicked
+  CLEAR_BUTTON.addEventListener('click', clearStyling);
+
+  function clearStyling() {
+    FILE_NAME.innerHTML = '';
+    UPLOAD_BOX.className = 'upload-box';
+    STATS_BUTTON.style.border = 'medium none';
+    STATS_BUTTON.style.cursor = 'auto';
+  }
 }
 
 uploadStyling();
